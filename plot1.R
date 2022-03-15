73075d8cb2bd33ce67e9d4b10f691fa2ad1d8432
Phuc Huy Vo <vphuchuy@gmail.com>
2022-03-15 20:24:15 +0700

    Commit

---
+++ plot1.R
@@ -0,0 +1,23 @@
+#Loading the data
+powerConsum <- read.csv("household_power_consumption.txt", sep = ";", stringsAsFactors = FALSE)
+
+#Convert the Date and Time variables to Date/Time classes
+powerConsum$Date <- as.Date(powerConsum$Date, format = "%d/%m/%Y")
+powerConsum$DateTime <- strptime(paste(powerConsum$Date,powerConsum$Time),format = "%Y-%m-%d %H:%M:%S")
+
+#Using data from the dates 2007-02-01 and 2007-02-02
+powerConsum <- powerConsum[powerConsum$Date >= "2007-2-1" & powerConsum$Date <= "2007-2-2",]
+
+#Convert the data to numeric for plotting
+powerConsum[,3] <- as.numeric(powerConsum[,3])
+powerConsum[,4] <- as.numeric(powerConsum[,4])
+powerConsum[,5] <- as.numeric(powerConsum[,5])
+powerConsum[,6] <- as.numeric(powerConsum[,6])
+powerConsum[,7] <- as.numeric(powerConsum[,7])
+powerConsum[,8] <- as.numeric(powerConsum[,8])
+
+#Constructing plot 1
+with(powerConsum, hist(Global_active_power, xlab = "Global Active Power (kilowatts)",col = "red", main = "Global Active Power"))
+
+#Save plot to PNG file
+dev.copy(device = png, width = 480, height = 480, file = "plot1.png")
---
+++ plot3.R
@@ -0,0 +1,32 @@
+#Loading the data
+powerConsum <- read.csv("household_power_consumption.txt", sep = ";", stringsAsFactors = FALSE)
+
+#Convert the Date and Time variables to Date/Time classes
+powerConsum$Date <- as.Date(powerConsum$Date, format = "%d/%m/%Y")
+powerConsum$DateTime <- strptime(paste(powerConsum$Date,powerConsum$Time),format = "%Y-%m-%d %H:%M:%S")
+
+#Using data from the dates 2007-02-01 and 2007-02-02
+powerConsum <- powerConsum[powerConsum$Date >= "2007-2-1" & powerConsum$Date <= "2007-2-2",]
+
+#Convert the data to numeric for plotting
+powerConsum[,3] <- as.numeric(powerConsum[,3])
+powerConsum[,4] <- as.numeric(powerConsum[,4])
+powerConsum[,5] <- as.numeric(powerConsum[,5])
+powerConsum[,6] <- as.numeric(powerConsum[,6])
+powerConsum[,7] <- as.numeric(powerConsum[,7])
+powerConsum[,8] <- as.numeric(powerConsum[,8])
+
+#Constructing plot 3
+with(powerConsum, plot(DateTime, Sub_metering_1, type = "n", xaxt = "n",
+                       xlab = "", ylab = "Energy sub metering"))
+axis(side = 1, at = c(as.numeric(powerConsum$DateTime[1]),
+                      as.numeric(powerConsum$DateTime[1441]),
+                      as.numeric(powerConsum$DateTime[2880])),labels = c("Thu","Fri","Sat"))
+with(powerConsum, lines(DateTime, Sub_metering_1))
+with(powerConsum, lines(DateTime, Sub_metering_2, col = "red"))
+with(powerConsum, lines(DateTime, Sub_metering_3, col = "blue"))
+legend("topright",lty = 1, col=c("black","red","blue"),
+       legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+
+#Save plot to a PNG file
+dev.copy(device = png, width = 480, height = 480, file = "plot3.png")
